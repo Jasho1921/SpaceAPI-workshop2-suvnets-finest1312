4cab86bdd99e60d937f7c438674a6ecb87977609
Jasho1921 <jasharmirzai@msn.com>
2026-01-29 10:28:10 +0100

    Added routes and diverse festligheter

--- SpaceController.cs
+++ SpaceController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
-[Route("api/v1")]
+[Route("/api/space")]
 public class SpaceController : ControllerBase
 {
     [HttpGet("hello")]
@@ -9,4 +9,35 @@ public class SpaceController : ControllerBase
     {
         return Ok("Hello from SpaceController!");
     }
+
+    [HttpGet("weather")]
+    public IActionResult GetWeatherFromSpace()
+    {
+        return Ok("Fint väder");
+    }
+
+    [HttpGet("location")]
+    public IActionResult GetLocation()
+    {
+        return Ok("info om borås? Eller bara att vi valt borås?");
+    }
+
+    [HttpGet("facts")]
+    public IActionResult GetLocationFunFact()
+    {
+        return Ok("5G strålningen är kraftig idag! Glöm ej foliehatten och paraply!");
+    }
+
+    // [HttpGet("norrsken")]
+    // public async Task<IActionResult> GetListOfAurora()
+    // {
+    //    // var cities = await _db.Cities.Select(c => där det finns norrsken just nu ).Where
+    // }
+    
 }
+
+
+// /api/space/weather
+// /api/space/location
+// /api/space/facts
+// /api/space/norrsken
