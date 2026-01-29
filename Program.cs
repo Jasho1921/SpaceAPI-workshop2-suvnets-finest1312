826df6e64d2ef44e8b1ca5c3f4ca4c2baccc00ab
Jasho1921 <jasharmirzai@msn.com>
2026-01-29 14:39:15 +0100

    Missed an important row in program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<SpaceDbContext>(options =>
@@ -21,26 +20,29 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Denna hör ihop med CORS-inställningen ovan
 app.UseCors();
 
-// using (var scope = app.Services.CreateScope())
-// {
-//     var context = scope.ServiceProvider.GetRequiredService<SpaceDbContext>();
+app.UseStaticFiles();
 
-//     if (!context.SatelliteImages.Any())
-//     {
-//         context.SatelliteImages.AddRange(
-//             new SatelliteImage { City = "Borås", ImageUrl = "/images/boras.jpg" },
-//             new SatelliteImage { City = "Stockholm", ImageUrl = "/images/stockholm.jpg" },
-//             new SatelliteImage { City = "Göteborg", ImageUrl = "/images/goteborg.jpg" }
-//         );
-//         context.SaveChanges();
-//     }
-// }
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<SpaceDbContext>();
+
+    if (!context.SatelliteImages.Any())
+    {
+        context.SatelliteImages.AddRange(
+            new SatelliteImage { City = "Borås", ImageUrl = "/images/boras.jpg" },
+            new SatelliteImage { City = "Stockholm", ImageUrl = "/images/stockholm.jpg" },
+            new SatelliteImage { City = "Göteborg", ImageUrl = "/images/goteborg.jpg" }
+        );
+        context.SaveChanges();
+    }
+}
 
 //Ni ska inte skriva era endpoints här i Program.cs utan i separata controllers, så använd denna:
 app.MapControllers();
